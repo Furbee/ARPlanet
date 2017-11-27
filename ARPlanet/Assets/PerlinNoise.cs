19c45171c2562ad61cf9e7060bd995ded3201fce
Oscar Nord <oscar.nord@gmail.com>
2017-11-27 11:24:39 +0100

    Updates

--- ARPlanet/Assets/PerlinNoise.cs
+++ ARPlanet/Assets/PerlinNoise.cs
@@ -5,11 +5,10 @@ using UnityEngine;
 public class PerlinNoise : MonoBehaviour {
 	public int width = 256;
 	public int height = 256;
-
 	public float scale = 20f;
 
 	void Start() {
-		Renderer renderer = GetComponent<Renderer> (); 
+		Renderer renderer = GetComponent<Renderer>(); 
 		renderer.material.mainTexture = GenerateTexture();
 	}
 
@@ -19,7 +18,7 @@ public class PerlinNoise : MonoBehaviour {
 		for(int x = 0; x < width; x++) {
 			for (int y = 0; y < height; y++) {
 				Color color = CalculateColor(x,y);
-				texture.SetPixel (x, y, color);
+				texture.SetPixel(x, y, color);
 			}	
 		}
 		texture.Apply();
@@ -29,7 +28,7 @@ public class PerlinNoise : MonoBehaviour {
 	Color CalculateColor(int x, int y) {
 		float xCoord = (float)x / width * scale;
 		float yCoord = (float)y / height * scale;
-		float sample = Mathf.PerlinNoise (xCoord, yCoord);
+		float sample = Mathf.PerlinNoise(xCoord, yCoord);
 		return new Color(sample, sample, sample);
 	}
 }
